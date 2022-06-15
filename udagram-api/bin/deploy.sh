a6b36beeb10eb38abbfe1229648379b332d06db7
Shorouk Elkhateeb <shrouca@gmail.com>
2022-06-15 21:47:39 +0200

    fix: Use eb init with the --platform option

--- udagram-api/bin/deploy.sh
+++ udagram-api/bin/deploy.sh
@@ -1,1 +1,1 @@
-eb init pathfinder --platform "arn:aws:elasticbeanstalk-us-east-1-174842930492::platform/Node.js 14 running on 64bit Amazon Linux 2/5.5.3" && eb use udagram-env && eb deploy
+eb init --platform "arn:aws:elasticbeanstalk:us-east-1::platform/Node.js 14 running on 64bit Amazon Linux 2/5.5.3" && eb use udagram-env && eb deploy
