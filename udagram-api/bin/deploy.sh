083bb6b22c3376c3a68efc72b086d9211fba7086
Shorouk Elkhateeb <shrouca@gmail.com>
2022-06-15 20:55:38 +0200

    fix: Automate eb init

--- udagram-api/bin/deploy.sh
+++ udagram-api/bin/deploy.sh
@@ -1,1 +1,1 @@
-eb init && eb use udagram-env && eb deploy
+eb init pathfinder --platform "arn:aws:s3:::elasticbeanstalk-us-east-1::platform/Node.js 14 running on 64bit Amazon Linux 2/5.5.3" && eb use udagram-env && eb deploy
